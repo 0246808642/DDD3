6aa5b3c9252a9c9a1ed704a1d9cecad253f3758f
caique  coelho pinheiro <caiquepinheiro69@gmai.com>
2024-03-19 20:34:20 -0300

    Add project files.

---
+++ DDD3.Infra.Data/Context/DDDModeloContext.cs
@@ -0,0 +1,17 @@
+﻿
+using DDD3.Domain.Entities;
+using System.Data.Entity;
+
+namespace DDD3.Infra.Data.Context
+{
+
+    public class DDDModeloContext : DbContext
+    {
+        public DDDModeloContext() 
+            : base("ProjetoModeloDDDTres")
+        {
+
+        }
+        public DbSet<Cliente> Clientes {  get; set; }
+    }
+}
---
+++ DDD3.Infra.Data/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace DDD3.Infra.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Context.DDDModeloContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Context.DDDModeloContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
